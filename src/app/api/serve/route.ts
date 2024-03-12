ec5c2932cc499c3ff93f68c776eb12884c21a3e0
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-03-12 22:32:48 +0000

    remove auth from `/api/serve`

--- src/app/api/serve/route.ts
+++ src/app/api/serve/route.ts
@@ -7,24 +7,8 @@ export async function GET(request: Request) {
   const format = searchParams.get('format') || 'webp'
   let quality: number = Number(searchParams.get('quality')) || 0
 
-  const apiKey = getApiKey(request)
-
-  if (!apiKey) {
-    return new Response('Error: api key not set', {
-      status: 400
-    })
-  }
-
   if (!url) {
     return new Response('Error: no image url provided', {
-      status: 400
-    })
-  }
-
-  const account = await getAccount(apiKey)
-
-  if (!account) {
-    return new Response('Error: account not found', {
       status: 400
     })
   }
