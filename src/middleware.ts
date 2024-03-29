add3f48a729dd3129f9f8277be0fdc24724a7b26
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-03-29 15:22:13 +0000

    Added rewrite for `assets.` subdomain to `/api/serve`

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import type { NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
   const accountKey = request.cookies.get('accountKey')?.value
@@ -6,8 +6,15 @@ export function middleware(request: NextRequest) {
   if (!accountKey && request.nextUrl.pathname.startsWith('/dashboard')) {
     return Response.redirect(new URL('/login', request.url))
   }
+
+  /**
+   * rewrite assets.imagize.io to imagize.io/api/serve
+   */
+  if (request.nextUrl.hostname === 'assets.imagize.jacksonlewis.dev') {
+    return NextResponse.rewrite(new URL('/api/serve', request.url))
+  }
 }
  
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!_next/static|_next/image|.*\\.png$).*)'],
 }
