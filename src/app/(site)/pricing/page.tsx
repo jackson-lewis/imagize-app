66f25eb03074a09b069e6d5d0d711e0f78919787
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-06-15 20:17:57 +0100

    Deep Stripe integration with products & subscriptions

--- src/app/(site)/pricing/page.tsx
+++ src/app/(site)/pricing/page.tsx
@@ -1,7 +1,17 @@
-export default function PricingPage() {
+import Plans from '@/components/site/plans'
+import { getPlans } from '@/lib/firebase-site'
+
+export default async function PricingPage() {
+  const plans = await getPlans()
+
+  if (!plans) {
+    return null
+  }
+
   return (
     <div>
       <h1>Pricing</h1>
+      <Plans data={plans} />
     </div>
   )
 }
